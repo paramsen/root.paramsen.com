02ea6b616d43ff1c42857974d745a2b55b6ac76a
Your Name <par.nils.amsen@gmail.com>
2016-09-07 17:07:54 +1000

    mobile first styling, flexbox ArticleList impl

--- modules/fe/code/src/component/ArticleList.js
+++ modules/fe/code/src/component/ArticleList.js
@@ -6,7 +6,7 @@ import {Card, CardMedia, CardTitle, CardText} from 'material-ui/Card'
 export default function ArticleList(props) {
     if(props.articles) {
         return (
-            <div>
+            <div className="article-list">
                 {renderArticleItems(props.articles)}
             </div>
         );
@@ -20,7 +20,7 @@ export default function ArticleList(props) {
 }
 
 function renderArticleItems(items) {
-    return items.map(renderArticleItemM);
+    return fillToMultipleOfThree(items.map(renderArticleItemM));
 }
 
 function renderArticleItem(item) {
@@ -43,8 +43,8 @@ function renderArticleItem(item) {
 
     function renderArticleItemM(item) {
     return(
-        <Link to={'/article/' + item.name} key={item.id}>
-          <Card className="card">
+        <Link to={'/article/' + item.name} key={item.id} className="card">
+          <Card>
             <CardMedia className="card-media">
                 <i className="card-icon fa fa-terminal" aria-hidden="true"></i>
             </CardMedia>
@@ -58,5 +58,31 @@ function renderArticleItem(item) {
             </CardText>
           </Card>
       </Link>
+    );
+}
+
+/**
+ * Add fill divs to have an amount of divs that is dividable by three, as we have 3 columns in UI at most
+ */
+function fillToMultipleOfThree(items) {
+    var multOfThree = items.length % 3;
+
+    if(multOfThree == 0) {
+        return items;
+    }
+
+    if(multOfThree == 1) {
+        items.push(cardFill(items.length));
+        items.push(cardFill(items.length));
+    } else {
+        items.push(cardFill(items.length));
+    }
+
+    return items;
+}
+
+function cardFill(id) {
+    return (
+        <div key={items.length+1} className="card card-fill"/>
     );
 }
