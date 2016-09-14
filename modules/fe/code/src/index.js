3ae0cee4b6585a51741c034e1f3d4fba76df267d
Your Name <par.nils.amsen@gmail.com>
2016-09-15 08:53:06 +1000

    nginx route /article to index.html, fix css webpack concat order

--- modules/fe/code/src/index.js
+++ modules/fe/code/src/index.js
@@ -7,10 +7,10 @@ import rootReducer from './reducer';
 import {browserHistory} from 'react-router';
 import createSagaMiddleware from 'redux-saga';
 import articleSaga from './saga/article';
-import AppRouter from './container';
-import injectTapEventPlugin from 'react-tap-event-plugin'; //for material-ui
 import './reset.css';
 import './index.css';
+import injectTapEventPlugin from 'react-tap-event-plugin'; //for material-ui
+import AppRouter from './container';
 
 import {routeLocationChange} from './action/routeAction';
 import {getArticles} from './action/articleAction';
